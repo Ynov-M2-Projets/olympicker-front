ce378077a39c90b47e2da0d5febd2aaca414c13c
Jérémy Gendre <jgendre@media-sante.com>
2021-12-13 09:45:13 +0100

    add base dependencies & main components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router";
+import Home from "./pages/Home";
+import Header from "./components/header/header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
+      <BrowserRouter>
+          <Header/>
+          <Routes>
+              <Route path="/" element={<Home/>} />
+          </Routes>
+          <Footer/>
+      </BrowserRouter>
   );
 }
 
