958c5f43eedba868512af60de0b5d8d6a688a11d
Martin PELCAT <martinpelcat@hotmail.com>
2022-01-05 15:11:06 +0100

    fix headers

--- src/utils/axios-client.js
+++ src/utils/axios-client.js
@@ -1,10 +1,14 @@
 import axiosInstance from 'axios';
-import {getToken} from "./token";
+import { getToken } from "./token";
 
 export const axios = axiosInstance.create({
     baseURL: 'https://olympicker.herokuapp.com'
 })
 
-export const axiosHeaders = () => ({
-    headers: {Authorization: `Bearer ${getToken()}`}
-})
+export const axiosHeaders = () => {
+    const token = getToken();
+    if (token) {
+        return { headers: { Authorization: `Bearer ${token}` } }
+    }
+    return {}
+}
